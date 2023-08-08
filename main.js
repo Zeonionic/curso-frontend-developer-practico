8850ff92e1d5412a80acc686ee6b073c18579d62
Zeonionic <88205568+Zeonionic@users.noreply.github.com>
2023-08-08 15:54:48 -0500

    Mobile and desktop menus were updated to be compatible with the Shopping cart

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
+// Menu Interfaz Desktop
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
+// Menu en Interfaz Mobile
 const burgerMenu = document.querySelector('.burgerMenu');
 const mobileMenu = document.querySelector('.mobile-menu')
 
+// Carrito De Compras
+const shoppingCart = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
+// Event Listeners
 navEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleCartMenu);
 
+// Event Functions
 function toggleDesktopMenu(){
+    aside.classList.add('inactive')
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCartMenu(){
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    aside.classList.toggle('inactive')
 }
